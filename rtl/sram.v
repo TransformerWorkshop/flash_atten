6589747dc51ceaab65147f78a11426d81436c234
Yucheng Sun <lambertsun84@outlook.com>
2026-04-19 11:41:07 +0800

    Map 64x32 PT SRAMs to TSMC macro for DC

--- rtl/sram.v
+++ rtl/sram.v
@@ -11,6 +11,42 @@ module sram #(
 	output reg  [ DATA_WIDTH-1:0] dout
 );
 
+`ifdef SYNTHESIS
+	localparam USE_TSMC_64X32 = (DATA_WIDTH == 32) && (DEPTH == 64);
+
+	generate
+		if (USE_TSMC_64X32) begin : gen_tsmc_sram
+			wire [DATA_WIDTH-1:0] macro_q;
+			wire                  macro_ceb = ~en;
+			wire                  macro_web = ~we;
+			wire [DATA_WIDTH-1:0] macro_bweb = we ? {DATA_WIDTH{1'b0}} : {DATA_WIDTH{1'b1}};
+
+			TEM5N28HPCPLVTA64X32M4SWSO u_tsmc_sram (
+				.SLP (1'b0      ),
+				.SD  (1'b0      ),
+				.A   (addr       ),
+				.D   (din        ),
+				.BWEB(macro_bweb ),
+				.Q   (macro_q    ),
+				.WEB (macro_web  ),
+				.CEB (macro_ceb  ),
+				.CLK (clk        )
+			);
+
+			always @(*) begin
+				if (en && !we) begin
+					dout = macro_q;
+				end else begin
+					dout = {DATA_WIDTH{1'b0}};
+				end
+			end
+		end else begin : gen_unsup_cfg
+			always @(*) begin
+				dout = {DATA_WIDTH{1'b0}};
+			end
+		end
+	endgenerate
+`else
 	reg [DATA_WIDTH-1:0] mem [0:DEPTH-1];
 
 	always @(posedge clk) begin
@@ -25,5 +61,6 @@ module sram #(
 			dout <= {DATA_WIDTH{1'b0}};
 		end
 	end
+`endif
 
 endmodule
